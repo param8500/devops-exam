43c162d466bfe9c600197470c80775a33b70c4b5
Parama Siva <addala.parama-siva@siemens.com>
2024-10-02 19:09:17 +0530

    added backend

--- provider.tf
+++ provider.tf
@@ -3,3 +3,10 @@ provider "aws" {
 }
 
 # Add your S3 backend configuration here
+terraform {
+  backend "s3" {
+    bucket = "467.devops.candidate.exam"
+    key    = "parama.siva"
+    region = "ap-south-1"
+  }
+}
