f05cd5e295aa566a8b426f826bbdecd7daa59854
Parama Siva <addala.parama-siva@siemens.com>
2024-10-03 10:31:07 +0530

    added providers block

--- provider.tf
+++ provider.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.69.0"
+    }
+  }
+}
+
 provider "aws" {
   region  = "ap-south-1" # Don't change the region
 }
+
 
 # Add your S3 backend configuration here
 terraform {
