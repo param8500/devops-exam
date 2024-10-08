eab354b982813eebe68b90b22e906948de69077c
Parama Siva <addala.parama-siva@siemens.com>
2024-10-08 16:53:02 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "aws_security_group_rule" "lambda" {
   from_port = 443
   to_port = 443
   protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.lambda.id
 }
 
